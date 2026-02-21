f42ecfc2b405a3166402bf9127def0c9a393cb45
manikandanmk2280 <manikandanmk2280@gmail.com>
2026-02-21 17:08:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def delete_bus(plate):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
